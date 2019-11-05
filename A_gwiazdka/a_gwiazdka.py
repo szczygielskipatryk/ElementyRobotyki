eb54aaa922637f511762a4bc6a53b0dfe0f630c1
Szczygiel <patryk8199@gmail.com>
2019-11-05 20:54:50 +0100

    dodanie grid.txt, zmiana wyswietlania mapy

--- A_gwiazdka/a_gwiazdka.py
+++ A_gwiazdka/a_gwiazdka.py
@@ -1,10 +1,10 @@
 import math
 import numpy as np
 import datetime
-from PIL import Image
+from PIL import Image, ImageOps
 
 __author__ = "Patryk Szczygielski"
-__version__ = "alpha 2.22"
+__version__ = "alpha 2.23"
 __email__ = "patryk8199@gmail.com"
 __desc__ = "Algorytm a-gwiazdka stworzony na podstawie wykładu z Elementów Robotyki Inteligentnej,oraz filmu na yt: " \
            "https://www.youtube.com/watch?v=eSOJ3ARN5FM dla lepszego zrozumienia działania algorytmu "
@@ -15,12 +15,13 @@ def heurastyka(p1, p2):
     y = p1[1] - p2[1]
     return math.sqrt(x ** 2 + y ** 2)
 
-#funckja sprawdzająca dzieci aktualnego punktu
+
+# funckja sprawdzająca dzieci aktualnego punktu
 def dzieci(pozycja, mapa):
     tablica = []
-    for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        dx = pozycja[0] + x
-        dy = pozycja[1] + y
+    for x in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        dx = pozycja[0] + x[0]
+        dy = pozycja[1] + x[1]
         if dx < 0 or dx > mapa.shape[0] - 1 or dy < 0 or dy > mapa.shape[1] - 1:
             continue
         if mapa[dx][dy] != 0:
@@ -28,7 +29,8 @@ def dzieci(pozycja, mapa):
         tablica.append((dx, dy))
     return tablica
 
-#wyjątek w przypadku braku droga
+
+# wyjątek w przypadku braku droga
 class BrakDrogi(Exception):
     pass
 
@@ -55,7 +57,7 @@ def a_gwiazdka(start, koniec, mapa):
             if aktualny is None or f[p] < aktualnyf:
                 aktualny = p
                 aktualnyf = f[p]
-#odtwarzanie drogi po tablicy(słowniku)rodziców
+        # odtwarzanie drogi po tablicy(słowniku)rodziców
         if aktualny == koniec:
             droga = set()
             droga.add(aktualny)
@@ -82,14 +84,20 @@ def a_gwiazdka(start, koniec, mapa):
     raise BrakDrogi
 
 
-def rysuj_mape(x, y, start, stop, trasa):
-    mapa = np.zeros((x, y, 3), dtype=np.uint8)
+def rysuj_mape(mapka, start, stop, trasa):
+    mapa = np.zeros((mapka.shape[0], mapka.shape[1], 3), dtype=np.uint8)
+    for x in range(mapka.shape[0]):
+        for y in range(mapka.shape[1]):
+            mapa[x][y] = [255, 255, 255]
+            if mapka[x][y] == 5:
+                mapa[x][y] = [0, 0, 0]
     for x in trasa:
-        mapa[x[0]][x[1]] = [0, 0, 225]
-    mapa[start[0]][start[1]] = [0, 225, 0]
+        mapa[x[0]][x[1]] = [0, 225, 225]
+    mapa[start[0]][start[1]] = [150, 225, 0]
     mapa[stop[0]][stop[1]] = [225, 0, 0]
     img = Image.fromarray(mapa, 'RGB')
     img = img.resize((512, 512), resample=0)
+    img = ImageOps.expand(img, 10, 'black')
     img.show()
 
 
@@ -103,12 +111,12 @@ def main():
         czask = datetime.datetime.now() - czas
         mapa2 = mapa
         for x in droga:
-            mapa2[x[0]][x[1]] = 2
-        mapa2[start[0]][start[1]] = 1
+            mapa2[x[0]][x[1]] = 1
+        mapa2[start[0]][start[1]] = 2
         mapa2[stop[0]][stop[1]] = 3
         print(mapa2)
         print(czask)
-        rysuj_mape(mapa.shape[0], mapa.shape[1], start, stop, droga)
+        rysuj_mape(mapa, start, stop, droga)
     except BrakDrogi:
         print("Nie znaleziono drogi")
 
